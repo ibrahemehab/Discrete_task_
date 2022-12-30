00ee647208509f7bf49d4747c7410085d65a91df
ibrahemehab <121694691+ibrahemehab@users.noreply.github.com>
2022-12-30 22:56:29 +0200

    Add files via upload

---
+++ Discrete2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input numbers from");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("To");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Perfect numbers between this numbers is");
+            for(;x<=y;x++)
+            {
+                int sum = 0;
+                for(int i =1;i<x;i++)
+                {
+                    if (x%i == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                if(x==sum)
+                {
+                    Console.WriteLine(X);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ discrete 1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+            int f1= int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the Second number");
+            int f2 = int.Parse(Console.ReadLine());
+            int i, sum;
+            for(int n=f1;n<=f2;n++)
+            {
+                i = 1;
+                sum = 0;
+                while(i<n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine("{0}", n);
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
